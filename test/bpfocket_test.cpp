1883346b827d705889d936f745db42b358e8bfc0
JHLeeeMe <lejung92@gmail.com>
2024-05-15 18:39:28 +0900

    [Add] utils namespace
    1. Add enum class `eResultCode`
    2. Update `RawSocket` class member variable & destructor (Incompletion)
    3. Add test cases (eResultCode, ioctl)
    4. typo

--- test/bpfocket_test.cpp
+++ test/bpfocket_test.cpp
@@ -5,6 +5,49 @@
 
 #include "bpfocket.h"
 
+TEST(sys, ioctl)
+{  // ::ioctl
+    using namespace ::bpfocket;
+
+    core::RawSocket sock{};
+    int sockfd = sock.fd();
+
+    struct ifconf ifc{};
+    ifc.ifc_len = 0;
+    ifc.ifc_buf = nullptr;
+
+    int retval = 0;
+    int len = 0;
+    {  // set ifc.ifc_len
+        retval = ::ioctl(sockfd, SIOCGIFCONF, &ifc);
+        ASSERT_EQ(0, retval);
+        ASSERT_EQ(nullptr, ifc.ifc_buf);
+
+        ASSERT_NE(0, ifc.ifc_len);
+        len = ifc.ifc_len;
+    }
+
+    std::vector<char> buf{};
+    {  // set all using ifc.ifc_len
+        buf.reserve(ifc.ifc_len);
+        buf.resize(ifc.ifc_len);
+        ifc.ifc_buf = buf.data();
+
+        retval = ::ioctl(sockfd, SIOCGIFCONF, &ifc);
+        ASSERT_EQ(len, ifc.ifc_len);
+
+        ASSERT_NE(nullptr, ifc.ifc_buf);
+    }
+}
+
+TEST(utils, eResultCode)
+{  // ::bpfocket::utils
+    using namespace ::bpfocket;
+
+    ASSERT_EQ(0, static_cast<int>(utils::eResultCode::Success));
+    ASSERT_EQ(10, static_cast<int>(utils::eResultCode::Failure));
+}
+
 TEST(core, RawSocket)
 {  // ::bpfocket::core
     using namespace ::bpfocket;
